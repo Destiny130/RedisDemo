f69312ef47654af642451528fb241b5ba6a2f412
Destiny130 <destiny1307654641@hotmail.com>
2019-04-11 21:51:02 +0800

    Update Program.cs

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace RedisDemo
 {
@@ -10,6 +12,19 @@ namespace RedisDemo
     {
         static void Main(string[] args)
         {
+            var conn = ConnectionMultiplexer.Connect("127.0.0.1:6370");
+            var db = conn.GetDatabase();
+            //db.StringSet("b", "abcdde");
+            //db.StringSet("a", "abb");
+
+            //var conn = ConnectionMultiplexer.Connect("127.0.0.1:6391");
+            //var db = conn.GetDatabase();
+            //Console.WriteLine(db.StringGet("a"));
+            //Console.WriteLine(db.StringGet("b"));
+            //Console.WriteLine(db.KeyDelete("a"));
+
+            Console.WriteLine("\nend");
+            Console.ReadKey();
         }
     }
 }
