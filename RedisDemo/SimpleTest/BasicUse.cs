e169d4419f95afa110a273b902a5cd6faccf3e25
Destiny130 <destiny1307654641@hotmail.com>
2019-04-15 17:01:58 +0800

    Update BasicUse.cs

--- RedisDemo/SimpleTest/BasicUse.cs
+++ RedisDemo/SimpleTest/BasicUse.cs
@@ -1,44 +1,49 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
-using StackExchange.Redis;
 using Newtonsoft.Json;
 using RedisDemo.RedisHelp;
-using System.Configuration;
+using System.Threading.Tasks;
 
 namespace RedisDemo.SimpleTest
 {
     public class BasicUse
     {
-        public void Execute()
+        private readonly RedisHelper redisConn;
+
+        public BasicUse(RedisHelper _redisConn)
+        {
+            this.redisConn = _redisConn;
+        }
+
+        public void ExecuteBatch()
         {
             Stopwatch watch = new Stopwatch();
-            string config = ConfigurationManager.ConnectionStrings["RedisSingle"].ConnectionString;
-            var conn = RedisConnectionHelp.GetConnectionMultiplexer(config);
-            int count = 50000;
+            int count = 30;
 
             watch.Start();
-            var db0 = conn.GetDatabase();
+            var db0 = redisConn.GetDatabase();
             var batch = db0.CreateBatch();
-            for (int i = 0; i < count; ++i)
-            {
-                batch.ListRightPushAsync("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone")));
-            }
 
-            Enumerable.Range(0, count).Select(i => batch.ListRightPushAsync("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone"))));
+            //List<Task<long>> batchTaskList = 
+            Enumerable.Range(0, count).Select(i => batch.ListRightPushAsync("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone")))).Count();
 
-            //var t = Enumerable.Range(0, 200000).Select(i => batch.ListRightPushAsync("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone"))));
             batch.Execute();
+            //watch.Stop();
+            //Console.WriteLine($"Batch.Execute() spent: {watch.ElapsedMilliseconds:F3} milliseconds");
+            //watch.Restart();
+            //await Task.WhenAll(batchTaskList);  //Why this stop spent some time? Is it check something?
+            //long sum = batchTaskList.Where(b => b.IsCompleted).Select(b => b.Result).Sum();
             watch.Stop();
+            //Console.WriteLine($"Task.WhenAll() spent: {watch.ElapsedMilliseconds:F3} milliseconds");
             Console.WriteLine($"Batch spent: {watch.ElapsedMilliseconds:F3} milliseconds");
+            //Console.WriteLine($"{String.Join(", ", batchTaskList.Where(e => e.IsCompleted).Select(e => e.Result))}");
 
             watch.Restart();
-            var db1 = conn.GetDatabase(1);
-            for (int i = 0; i < count; ++i)
-            {
-                db1.ListRightPush("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone")));
-            }
-            //Enumerable.Range(0, 200000).Select(i => db1.ListRightPush("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone"))));
+            var db1 = redisConn.GetDatabase(1);
+            Enumerable.Range(0, count).Select(i => db1.ListRightPush("Person", JsonConvert.SerializeObject(new Person(i, i.ToString() + " first", i.ToString() + " last", i.ToString() + " address", i.ToString() + " phone")))).Count();
             watch.Stop();
             Console.WriteLine($"Normal spent: {watch.ElapsedMilliseconds:F3} milliseconds");
         }
