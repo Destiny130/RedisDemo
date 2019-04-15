0809ddd17cb33d3a28250d8156c2bd86fbb803ed
Destiny130 <destiny1307654641@hotmail.com>
2019-04-15 17:01:09 +0800

    Update RedisConnectionHelp.cs

--- RedisDemo/RedisHelp/RedisConnectionHelp.cs
+++ RedisDemo/RedisHelp/RedisConnectionHelp.cs
@@ -1,70 +1,63 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
-using System.Configuration;
 using StackExchange.Redis;
 
 namespace RedisDemo.RedisHelp
 {
-    /// <summary>
-    /// ConnectionMultiplexer对象管理帮助类
-    /// </summary>
     public static class RedisConnectionHelp
     {
-        //系统自定义Key前缀
-        public static readonly string SysCustomKey = ConfigurationManager.AppSettings["redisKey"] ?? "";
+        private static readonly string redisConnectionString = Util.ReadConnectionString("RedisCluster", "127.0.0.1:6370");
+        public static readonly string sysCustomKey = Util.ReadAppSetting("RedisKey", String.Empty);
 
-        //"127.0.0.1:6379,allowadmin=true
-        private static readonly string RedisConnectionString = ConfigurationManager.ConnectionStrings["RedisSingle"].ConnectionString;
+        private static readonly object locker = new object();
+        private static ConnectionMultiplexer instance;
+        private static readonly ConcurrentDictionary<string, ConnectionMultiplexer> connectionCache = new ConcurrentDictionary<string, ConnectionMultiplexer>();
 
-        private static readonly object Locker = new object();
-        private static ConnectionMultiplexer _instance;
-        private static readonly ConcurrentDictionary<string, ConnectionMultiplexer> ConnectionCache = new ConcurrentDictionary<string, ConnectionMultiplexer>();
-
-        /// <summary>
-        /// Get singletion
-        /// </summary>
         public static ConnectionMultiplexer Instance
         {
             get
             {
-                if (_instance == null || !_instance.IsConnected)
+                if (instance == null || !instance.IsConnected)
                 {
-                    lock (Locker)
+                    lock (locker)
                     {
-                        if (_instance == null || !_instance.IsConnected)
+                        if (instance == null || !instance.IsConnected)
                         {
-                            _instance = GetManager();
+                            //if (!connectionCache.ContainsKey(redisConnectionString) || !connectionCache[redisConnectionString].IsConnected)
+                            //{
+                            //    connectionCache[redisConnectionString] = GetManager(redisConnectionString);
+                            //    instance = connectionCache[redisConnectionString];
+                            //}
+                            //else
+                            //{
+                            //    instance = connectionCache[redisConnectionString];
+                            //}
+                            instance = GetManager();
                         }
                     }
                 }
-                return _instance;
+                return instance;
             }
         }
 
-        /// <summary>
-        /// Read cache
-        /// </summary>
-        /// <param name="connectionString"></param>
-        /// <returns></returns>
         public static ConnectionMultiplexer GetConnectionMultiplexer(string connectionString)
         {
-            if (!ConnectionCache.ContainsKey(connectionString))
+            if (!connectionCache.ContainsKey(connectionString))
             {
-                ConnectionCache[connectionString] = GetManager(connectionString);
+                connectionCache[connectionString] = GetManager(connectionString);
             }
-            return ConnectionCache[connectionString];
+            return connectionCache[connectionString];
         }
 
         private static ConnectionMultiplexer GetManager(string connectionString = null)
         {
-            connectionString = connectionString ?? RedisConnectionString;
+            connectionString = connectionString ?? redisConnectionString;
             var connect = ConnectionMultiplexer.Connect(connectionString);
 
-            //注册如下事件
+            connect.ConfigurationChanged += MuxerConfigurationChanged;
             connect.ConnectionFailed += MuxerConnectionFailed;
             connect.ConnectionRestored += MuxerConnectionRestored;
             connect.ErrorMessage += MuxerErrorMessage;
-            connect.ConfigurationChanged += MuxerConfigurationChanged;
             connect.HashSlotMoved += MuxerHashSlotMoved;
             connect.InternalError += MuxerInternalError;
 
@@ -73,64 +66,34 @@ namespace RedisDemo.RedisHelp
 
         #region Event
 
-        /// <summary>
-        /// 配置更改时
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private static void MuxerConfigurationChanged(object sender, EndPointEventArgs e)
         {
-            Console.WriteLine("Configuration changed: " + e.EndPoint);
+            Console.WriteLine($"{nameof(MuxerConfigurationChanged)}: " + e.EndPoint);
         }
 
-        /// <summary>
-        /// 发生错误时
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
-        {
-            Console.WriteLine("ErrorMessage: " + e.Message);
-        }
-
-        /// <summary>
-        /// 重新建立连接之前的错误
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
-        {
-            Console.WriteLine("ConnectionRestored: " + e.EndPoint);
-        }
-
-        /// <summary>
-        /// 连接失败 ， 如果重新连接成功你将不会收到这个通知
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private static void MuxerConnectionFailed(object sender, ConnectionFailedEventArgs e)
         {
-            Console.WriteLine("重新连接：Endpoint failed: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)));
+            Console.WriteLine($"{nameof(MuxerConnectionFailed)}: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)));
         }
 
-        /// <summary>
-        /// 更改集群
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine($"{nameof(MuxerConnectionRestored)}: " + e.EndPoint);
+        }
+
+        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+            Console.WriteLine($"{nameof(MuxerErrorMessage)}: " + e.Message);
+        }
+
         private static void MuxerHashSlotMoved(object sender, HashSlotMovedEventArgs e)
         {
-            Console.WriteLine("HashSlotMoved:NewEndPoint" + e.NewEndPoint + ", OldEndPoint" + e.OldEndPoint);
+            Console.WriteLine($"{ nameof(MuxerHashSlotMoved)}. NewEndPoint: " + e.NewEndPoint + ", OldEndPoint: " + e.OldEndPoint);
         }
 
-        /// <summary>
-        /// redis类库错误
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private static void MuxerInternalError(object sender, InternalErrorEventArgs e)
         {
-            Console.WriteLine("InternalError:Message" + e.Exception.Message);
+            Console.WriteLine($"{nameof(MuxerInternalError)}: " + e.Exception.Message);
         }
 
         #endregion Event
