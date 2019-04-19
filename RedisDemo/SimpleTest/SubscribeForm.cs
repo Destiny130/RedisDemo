9c8952aed3777bcb4693fa4b5261ef39aeb20a37
Destiny130 <destiny1307654641@hotmail.com>
2019-04-19 16:56:05 +0800

    Update SubscribeForm.cs

--- RedisDemo/SimpleTest/SubscribeForm.cs
+++ RedisDemo/SimpleTest/SubscribeForm.cs
@@ -6,6 +6,7 @@ using RedisDemo.RedisHelp;
 using StackExchange.Redis;
 using System.Threading;
 using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace RedisDemo.SimpleTest
 {
@@ -15,6 +16,7 @@ namespace RedisDemo.SimpleTest
 
         Label publishLabel;
         Button publishButton;
+        Button cleanButton;
         RichTextBox publishArea;
 
         Label subscribeLabel;
@@ -24,13 +26,15 @@ namespace RedisDemo.SimpleTest
 
         RedisHelper redis;
         SynchronizationContext context;  //B: commit a request
-        BackgroundWorker worker = null;  //D: BackgroundWorker
+        //BackgroundWorker worker = null;  //D: BackgroundWorker
 
         public SubscribeForm(RedisHelper _redis)
         {
             publishLabel = new Label() { Location = new Point(10, 15), Height = 10, Width = 120, Text = "Publish Content" };
             publishButton = new Button() { Location = new Point(130, 11), Height = 20, Width = 100, Text = "Publish" };
             publishButton.Click += PublishAsync;
+            cleanButton = new Button() { Location = new Point(400, 11), Height = 20, Width = 100, Text = "Clean All" };
+            cleanButton.Click += (sender, e) => { publishArea.Clear(); subscribeArea.Clear(); };
             publishArea = new RichTextBox() { Location = new Point(10, 40), Height = 100, Width = 800, ReadOnly = true };
             //publishArea.TextChanged += PublishAreaScrollToEnd;
             publishArea.TextChanged += (sender, e) => publishArea.ScrollToCaret();
@@ -44,9 +48,10 @@ namespace RedisDemo.SimpleTest
             //subscribeArea.TextChanged += SubscribeAreaScrollToEnd;
             subscribeArea.TextChanged += (sender, e) => subscribeArea.ScrollToCaret();
 
-            Control[] controlArr = new Control[] { publishLabel, publishButton, publishArea, subscribeLabel, subscribeButton, unsubscribeButton, subscribeArea };
+            Control[] controlArr = new Control[] { publishLabel, publishButton, cleanButton, publishArea, subscribeLabel, subscribeButton, unsubscribeButton, subscribeArea };
             Controls.AddRange(controlArr);
             AutoSize = true;
+            CenterToScreen();
 
             redis = _redis;
 
@@ -57,49 +62,56 @@ namespace RedisDemo.SimpleTest
             context = SynchronizationContext.Current;
 
             //D: BackgroundWorker
-            worker = new BackgroundWorker();
-            worker.WorkerReportsProgress = true;
-            worker.DoWork += (sender, e) => worker.ReportProgress(50, "Worker is running");
-            worker.ProgressChanged += (sender, e) => subscribeArea.AppendText(e.UserState.ToString());
-            //worker.RunWorkerCompleted += (sender, e) => subscribeArea.AppendText(e.Result.ToString());
-            worker.RunWorkerCompleted += (sender, e) => subscribeArea.AppendText($"{e.Result?.ToString()}\n");
+            //worker = new BackgroundWorker();
+            //worker.WorkerReportsProgress = true;
+            //worker.DoWork += (sender, e) => worker.ReportProgress(50, "Worker is running");
+            //worker.ProgressChanged += (sender, e) => subscribeArea.AppendText(e.UserState.ToString());
+            //worker.RunWorkerCompleted += (sender, e) => subscribeArea.AppendText($"{e.Result?.ToString()}\n");
         }
 
         #region Events
 
         async void SubscribeAsync(object sender, EventArgs e)
         {
-            //A: unsafe way
+            #region A: unsafe way
+
             //Action<RedisChannel, RedisValue> handlerA = (channel, message) => subscribeArea.AppendText(subscribePrint(channel, message, "A"));
             //await redis.SubscribeAsync(dummyChannel, handlerA);
 
-            //B: commit a post request
-            SendOrPostCallback callback = obj => subscribeArea.AppendText(obj.ToString());
-            Action<RedisChannel, RedisValue> handlerB = (channel, message) => context.Post(callback, subscribePrint(channel, message, "B"));
-            await redis.SubscribeAsync(dummyChannel, handlerB);
+            #endregion A: unsafe way
 
-            //C1: invoke
-            Action<RedisChannel, RedisValue> handlerC1 = (channel, message) =>
-            {
-                string output = subscribePrint(channel, message, "C1");
-                if (subscribeArea.InvokeRequired)
-                {
-                    while (!subscribeArea.IsHandleCreated)
-                    {
-                        if (subscribeArea.Disposing || subscribeArea.IsDisposed)
-                            return;
-                    }
-                    Action<string> action = text => subscribeArea.AppendText(text);
-                    Invoke(action, new object[] { output });  //Will cause performance problem
-                }
-                else
-                {
-                    subscribeArea.AppendText(output);
-                }
-            };
-            await redis.SubscribeAsync(dummyChannel, handlerC1);
+            #region B: commit a post request
 
-            //C2: async invoke
+            //SendOrPostCallback postCallback = obj => subscribeArea.AppendText(obj.ToString());
+            //Action<RedisChannel, RedisValue> handlerB = (channel, message) => context.Post(postCallback, subscribePrint(channel, message, "B"));
+            //await redis.SubscribeAsync(dummyChannel, handlerB);
+
+            #endregion B: commit a post request
+
+            #region C: invoke
+
+            ////Sync invoke
+            //Action<RedisChannel, RedisValue> handlerC1 = (channel, message) =>
+            //{
+            //    string output = subscribePrint(channel, message, "C1");
+            //    if (subscribeArea.InvokeRequired)
+            //    {
+            //        while (!subscribeArea.IsHandleCreated)
+            //        {
+            //            if (subscribeArea.Disposing || subscribeArea.IsDisposed)
+            //                return;
+            //        }
+            //        Action<string> action = text => subscribeArea.AppendText(text);
+            //        Invoke(action, new object[] { output });  //Will cause performance problem
+            //    }
+            //    else
+            //    {
+            //        subscribeArea.AppendText(output);
+            //    }
+            //};
+            //await redis.SubscribeAsync(dummyChannel, handlerC1);
+
+            ////Async invoke
             Action<RedisChannel, RedisValue> handlerC2 = (channel, message) =>
             {
                 string output = subscribePrint(channel, message, "C2");
@@ -119,14 +131,32 @@ namespace RedisDemo.SimpleTest
                     subscribeArea.AppendText(output);
                 }
             };
-            await redis.SubscribeAsync(dummyChannel, handlerC2);
+            //await redis.SubscribeAsync(dummyChannel, handlerC2);
 
-            //D: BackgroundWorker
-            Action<RedisChannel, RedisValue> handlerD = (channel, message) =>
+            #endregion C: invoke
+
+            #region D: BackgroundWroker
+
+            //Action<RedisChannel, RedisValue> handlerD = (channel, message) =>
+            //{
+            //    worker.RunWorkerAsync(subscribePrint(channel, message, "D"));
+            //};
+            //await redis.SubscribeAsync(dummyChannel, handlerD);
+
+            #endregion D: BackgroundWroker
+
+            #region E: multiply threads 
+
+            List<Task> taskList = new List<Task>();
+            Parallel.For(0, 10, i =>
             {
-                worker.RunWorkerAsync(subscribePrint(channel, message, "D"));
-            };
-            await redis.SubscribeAsync(dummyChannel, handlerD);
+                //taskList.Add(redis.SubscribeAsync(dummyChannel, handlerC2));
+                //taskList.Add(new Task(() => redis.Subscribe(dummyChannel, handlerC2)));
+                taskList.Add(Task.Run(() => redis.Subscribe(dummyChannel, handlerC2)));
+            });
+            await Task.WhenAll(taskList);
+
+            #endregion
 
             subscribeButton.Enabled = false;
             unsubscribeButton.Enabled = true;
@@ -148,10 +178,6 @@ namespace RedisDemo.SimpleTest
         }
 
         #endregion Events
-
-        #region Help methods
-
-        #endregion Help methods
 
         #region Delegates
 
